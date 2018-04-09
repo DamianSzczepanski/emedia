5bc24bb68f2586b517266c7116f042c167afd071
Damian <damianszczepanski1@gmail.com>
2018-04-09 13:09:06 +0200

    emedia

---
+++ WindowsFormsApp2/Decode.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+
+    public class Decode
+    {
+        public int dlugosc, precyzja, wysokosc, szerokosc;
+
+
+        public void Dekoduj_Wlasnosci(byte[] Obrazek)
+        {
+            for (int i = 0; i < Obrazek.Length; i++)
+            {
+                if (Obrazek[i] == 0xff && Obrazek[i + 1] == 0xc0)
+                {
+                    Console.WriteLine("Wykryto Znacznik S0F0");
+                    dlugosc = (Obrazek[i + 2]) * 256 + Obrazek[i + 3];
+                    precyzja = (Obrazek[i + 4]);
+                    wysokosc = (Obrazek[i + 5] * 256 + Obrazek[i + 6]);
+                    szerokosc = (Obrazek[i + 7] * 256 + Obrazek[i + 8]);
+                }
+                else if (Obrazek[i] == 0xff && Obrazek[i + 1] == 0xd8)
+                {
+                    Console.WriteLine("Wykryto Plik jpg, rozpoczynam dekodowanie");
+                }
+            }
+        }
+
+        public double[] Dekodujplik(byte[] Obrazek)
+        {
+            double[] obrazek1;
+            obrazek1 = new double[Obrazek.Length];
+            for (int i = 0; i < Obrazek.Length; i++)
+            {
+                obrazek1[i] = (double)Obrazek[i];
+                //Console.WriteLine(obrazek1[i]);
+            }
+            return obrazek1;
+        }
+
+
+
+
+    }
+}
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Wyswietl_Wlasnosci(Decode a)
+        {
+           
+            Console.WriteLine("Dlugosc: " + a.dlugosc);
+            Console.WriteLine("Precyzja: " +a. precyzja);
+            Console.WriteLine("Wysokosc: " + a.wysokosc);
+            Console.WriteLine("Szerokosc: " + a.szerokosc);
+            label1.Text = "Dlugosc:" + a.dlugosc;
+            label2.Text = "Prezycja:" + a.precyzja;
+            label3.Text = "Wysokosc:" + a.wysokosc;
+            label4.Text = "Szerokosc:" + a.szerokosc;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] Obrazek = File.ReadAllBytes("picture.jpg");
+            Bitmap ob=new Bitmap ("picture.jpg");
+            FFT fft=new FFT(ob);
+            Decode dekoduj = new Decode();
+            dekoduj.Dekoduj_Wlasnosci(Obrazek);
+            Wyswietl_Wlasnosci(dekoduj);
+            //fft.Displayimage();
+            //fft.FFTPlot();
+            //pictureBox1.Load("ob");
+            //pictureBox1.Image = Image.FromFile("picture1.jpg");
+            fft.ForwardFFT();
+            fft.FFTPlot();
+            Bitmap ob1;
+            ob1 = fft.Displayimage();
+            pictureBox1.Size = new Size(dekoduj.szerokosc, dekoduj.wysokosc);
+            pictureBox1.Image = ob1;
+            //Console.ReadKey();
+        }
+    }
+}
